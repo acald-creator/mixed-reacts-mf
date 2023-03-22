47c5b48f61fc50e1d94966b7dda1a80194430e57
Antonette Caldwell <pullmana8@gmail.com>
2023-03-22 09:48:26 -0500

    added cloud remote button

--- dashboard/src/async-pages/index.tsx
+++ dashboard/src/async-pages/index.tsx
@@ -3,11 +3,17 @@ import dynamic from 'next/dynamic';
 import Box from '@/components/Box';
 import { styled } from '../../stitches.config';
 
-const RemoteButton = dynamic(() => 
+const RemoteButton = dynamic(() =>
   // @ts-ignore
   import('remote/Button'), {
   ssr: false,
 });
+
+const CloudRemoteButton = dynamic(() =>
+  // @ts-ignore
+  import('cloud/Button'), {
+  ssr: false,
+})
 
 const Text = styled('p', {
   fontFamily: '$system',
@@ -22,6 +28,8 @@ export default function Home() {
       <Button />
       <Text as="h2">Client - Button</Text>
       <RemoteButton />
+      <Text as="h2">Client - Cloud Button</Text>
+      <CloudRemoteButton />
     </Box>
   );
 }
