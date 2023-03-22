24cce7728840519d694aa2fe37a4633a3e59b772
Antonette Caldwell <pullmana8@gmail.com>
2023-03-22 09:41:35 -0500

    setup cloud for module federation

--- cloud/craco.config.js
+++ cloud/craco.config.js
@@ -1,5 +1,46 @@
 const CracoSwcPlugin = require('craco-swc')
+const { dependencies } = require("./package.json")
+const { ModuleFederationPlugin } = require('webpack').container
 
 module.expots = {
-    plugins: [{ plugin: CracoSwcPlugin }]
+    plugins: [
+        { 
+            plugin: CracoSwcPlugin,
+            options: {
+                swcLoaderOptions: {
+                    jsx: {
+                        parser: {
+                            syntax: "typescript",
+                            tsx: true,
+                            dynamicImport: true,
+                            exportDefaultForm: true,
+                        },
+                    }
+                }
+            }
+        }
+    ],
+    webpack: {
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'cloud',
+                filename: 'remote.js',
+                exposes: {
+                    "./CloudButton": "./src/components/Button",
+                },
+                shared: {
+                    ...dependencies,
+                    '@stitches/react': {
+                        singleton: true,
+                    },
+                    'react-dom': {
+                        import: 'react-dom',
+                        shareKey: 'react-dom',
+                        shareScope: 'legacy',
+                        singleton: true,
+                    }
+                }
+            })
+        ]
+    }
 }
