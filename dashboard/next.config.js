87fcc4afaa95d4f2189352ac458da88b25497e31
Antonette Caldwell <pullmana8@gmail.com>
2023-03-22 09:48:14 -0500

    update dashboard to add cloud as a remote component

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -10,13 +10,24 @@ const nextConfig = {
       //config.cache=false
       config.plugins.push(
         new NextFederationPlugin({
-          name: 'checkout',
+          name: 'dashboard',
           remotes: {
             remote: 'remote@http://localhost:8081/remote.js',
+            cloud: 'cloud@http://localhost:8082/remote.js',
           },
           filename: 'static/chunks/remoteEntry.js',
-          exposes: {},
-          shared: {},
+          exposes: {
+            './newReact': require.resolve('react'),
+            './newReactDOM': require.resolve('react-dom'),
+          },
+          shared: {
+            'react': {
+              import: 'react',
+              shareKey: 'newReact',
+              shareScope: 'default',
+              singleton: true,
+            }
+          }
         }),
       );
     }
